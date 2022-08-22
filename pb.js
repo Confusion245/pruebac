5a786eb4e23a8b880869e84f0b49d36941c31d91
Confusion245 <confumodsjsjsjxd@gmail.com>
2022-08-22 18:38:17 -0400

    Iniciando... ConfuMods UwU

---
+++ pb.js
@@ -0,0 +1,19 @@
+const canvacord = require("canvacord");
+
+//const userData = getDataSomehow();
+const img = "https://cdn.discordapp.com/embed/avatars/0.png";
+
+          const rank = new canvacord.Rank()
+            .setAvatar(img)
+            .setCurrentXP(203)
+            .setRequiredXP(500)
+            .setStatus("dnd")
+            .setProgressBar(["#FF0000", "#0000FF"], "GRADIENT")
+            .setUsername("Snowflake")
+            .setDiscriminator("0007");
+        
+        rank.build()
+            .then(data => {
+                canvacord.write(data, "RankCard.png");
+            })    
+    
